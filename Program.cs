0108a965c722d04518c7202bc6851b51488a137e
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-03 20:18:00 +0700

    add project lab3

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chieu rong hinh chu nhat");
+        double width = double.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap chieu cao hinh chu nhat");
+        double height = double.Parse(Console.ReadLine());  
+        double chuvi = (width + height) * 2;
+        double dientich = width * height;
+        Console.WriteLine($"Chu vi hinh chu nhat la: {chuvi}");
+        Console.WriteLine($"Dien tich hinh chu nhat la: {dientich}");
+    }
+}
